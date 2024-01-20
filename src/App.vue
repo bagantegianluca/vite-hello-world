6079fcf15e026686422d5eef3a8cea0a252b8be8
bagantegianluca <bagantegianluca@gmail.com>
2024-01-20 13:03:46 +0100

    Test commit by terminal

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      firstName: 'Gianluca',
+      content: 'lorem ipsum color'
+    }
+  },
+  methods:{
+    functionLog(){
+      console.log('Test');
+      this.functionChild();
+    },
+    functionChild(){
+      console.log('Child function');
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hello {{ firstName }}</h1>
+    <p>{{ content }}</p>
+    <button @click="functionLog">Click</button>
+  </div>
+
+</template>
+
+<style>
+</style>
